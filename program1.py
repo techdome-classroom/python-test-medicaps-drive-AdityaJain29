a12331a46522ed3e0bdacc754d46d05aa9bf00c6
AdityaJain29 <99635936+AdityaJain29@users.noreply.github.com>
2024-04-25 13:24:00 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -23,25 +23,7 @@ class Solution(object):
     
 
 
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        stack = []
-        mapping = {')': '(', '}': '{', ']': '['}
-        
-        for char in s:
-            if char in mapping.values():
-                stack.append(char)
-            elif char in mapping.keys():
-                if not stack or mapping[char] != stack.pop():
-                    return False
-            else:
-                return False
-        
-        return len(stack) == 0
+
 
 # Example usage:
 solution = Solution()
