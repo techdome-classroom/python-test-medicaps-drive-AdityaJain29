f11a975b30bb815fc9cadea6100d5e7815974475
AdityaJain29 <99635936+AdityaJain29@users.noreply.github.com>
2024-04-25 13:23:55 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,6 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+
+                stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
+                    return False
+            else:
+                return False
+        
+        return len(stack) == 0
+    
         pass
     
 
