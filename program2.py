58350f20af24705430492a347bb726d60cb154b1
AdityaJain29 <99635936+AdityaJain29@users.noreply.github.com>
2024-04-25 13:35:41 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -26,11 +26,3 @@ class Solution(object):
             prev_value = value
         
         return total
-
-# Example usage:
-solution = Solution()
-print(solution.romanToInt("III"))  # Output: 3
-print(solution.romanToInt("IV"))   # Output: 4
-print(solution.romanToInt("IX"))   # Output: 9
-print(solution.romanToInt("LVIII"))  # Output: 58
-print(solution.romanToInt("MCMXCIV"))  # Output: 1994
